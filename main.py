ef53e82bf766189076c50e175c6fc28dd46b3a8b
Xql.dev <205849173+KirobotDev@users.noreply.github.com>
2026-07-19 10:19:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import subprocess
 import psutil
 from datetime import datetime
 import winreg
-import cpuinfo
 import GPUtil
 import wmi
 from colorama import Fore
@@ -26,6 +25,19 @@ def fastfetch() -> str:
     name = socket.gethostname()
     ip = socket.gethostbyname(name)
 
+    result = subprocess.check_output(
+    [
+        "powershell",
+        "-Command",
+        "Get-CimInstance Win32_VideoController | Select-Object -ExpandProperty Name"
+        ]
+    )
+
+    gpu_names = result.decode("utf-8").splitlines()
+
+    for gpu in gpu_names:
+        gpp = gpu
+
     os.system("cls")
     print(f"""{Fore.BLUE}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀             {Fore.BLUE} {Fore.WHITE} ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 {Fore.BLUE}                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⡿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀{Fore.BLUE} {Fore.WHITE} ⠀⠀⠀
@@ -42,7 +54,7 @@ def fastfetch() -> str:
 {Fore.BLUE}                ⠀⠀⠀⠀⠀⠀⠀⠀⣼⢿⠟⠀⠀⠉⠉⠉⢀⣶⣶⣶⣶⠆⠀⠙⠛⠛⠓⢃⣾⣿⣿⣿⣿⢧⢿⡸⡱⢎⠶⣙⠼⣘⣳⣾⣿⣿⠋⠀⠠⠀⡐⠀⡁⠄⢀⢹⣿⣿⣿⣿⡟{Fore.BLUE} {Fore.WHITE}       {Fore.GREEN}Os{Fore.WHITE} : {Fore.BLUE}{oss}{Fore.WHITE}
 {Fore.BLUE}                ⠀⠀⠀⠚⠊⠀⠀⠀⠀⠀⠀⢠⣤⢤⡄⠀⠚⠛⠛⠿⠏⣰⣿⣷⣶⣶⠶⣾⣻⣻⣟⣻⢯⡟⢦⡓⡝⣎⠳⣍⢞⣱⣿⣿⣿⠏⠀⠠⠁⠠⠀⠐⠀⡀⢂⣾⣿⣿⣿⡟⠀{Fore.BLUE} {Fore.WHITE}       {Fore.GREEN}Uptime{Fore.WHITE} : {Fore.BLUE}{aff}{Fore.WHITE}
 {Fore.BLUE}                ⠀⠀⠀⠀⠀⠀⣀⣶⡆⠀⠀⠿⠿⠿⠁⢀⣀⡀⠀⠀⢰⣿⣿⣿⣿⡏⣹⣿⣿⣿⣿⡏⣿⣿⣿⣿⣿⣾⣷⣎⣎⣿⣿⣿⡏⠀⢀⠁⠰⠀⠁⢀⠁⠀⣾⣿⣿⣿⡿⠀⠀{Fore.BLUE} {Fore.WHITE}       {Fore.GREEN}Local_IP{Fore.WHITE} : {Fore.BLUE}{ip}{Fore.WHITE}
-{Fore.BLUE}                ⢀⣤⡤⠀⠀⠀⠛⠛⠁⠀⠀⠀⠀⠀⢠⣟⣤⣩⠟⠀⣀⡀⠉⠉⢉⣼⣿⣿⣿⣿⡟⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣀⠀⠂⠠⠐⠀⡁⠠⠀⣼⣿⣿⣿⡿⠁⠀ {Fore.BLUE} {Fore.WHITE} 
+{Fore.BLUE}                ⢀⣤⡤⠀⠀⠀⠛⠛⠁⠀⠀⠀⠀⠀⢠⣟⣤⣩⠟⠀⣀⡀⠉⠉⢉⣼⣿⣿⣿⣿⡟⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣀⠀⠂⠠⠐⠀⡁⠠⠀⣼⣿⣿⣿⡿⠁⠀ {Fore.BLUE} {Fore.WHITE}       {Fore.GREEN}GPU{Fore.WHITE} : {Fore.BLUE}{gpp}{Fore.WHITE}
 {Fore.BLUE}                ⠀⠉⠁⠀⣀⠀⡀⠀⠠⣾⣿⡿⠃⠀⠀⠀⠀⠉⠀⣼⠉⠭⣹⠏⣸⣷⣶⣶⣶⡞⣽⠣⡰⡐⠦⡘⡌⠭⣙⣿⣿⣿⣿⣿⣿⣷⣦⣁⠀⢂⠠⠐⣼⣿⣿⣿⡿⠁⠀⠀{Fore.BLUE} {Fore.WHITE} 
 {Fore.BLUE}                ⠀⠀⠀⠸⠿⠿⠃⠀⠀⠀⠀⠀⢠⡞⡍⢳⡞⠀⠈⠉⠙⠛⠋⣰⣿⣿⣿⣿⡟⣼⠣⢱⢡⡙⢦⠱⣘⢰⣿⣿⣿⡟⠀⠉⠛⢿⣿⣿⣷⣤⡀⣼⣿⣿⣿⡿⠃⠀⠀⠀{Fore.BLUE} {Fore.WHITE} 
 {Fore.BLUE}                ⠀⠀⠀⠀⠀⠀⣠⣶⣶⣶⠆⠀⠈⠉⠙⠋⠀⡴⣋⠛⣳⠖⣺⣭⣭⣭⣭⡿⣶⢡⢃⠇⢦⡑⢎⡑⢢⣿⣿⣿⡟⠀⠀⠀⠀⠀⠈⠻⢿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀{Fore.BLUE} {Fore.WHITE} 
